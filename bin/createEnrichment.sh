85735fac228499359d66952e74b83facc3e4efcb
Tadeu Tupinambá. <tadeu.tupiz@gmail.com>
2024-12-04 13:00:38 -0300

    bin folder

--- bin/createEnrichment.sh
+++ bin/createEnrichment.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -X POST https://9i82rka097.execute-api.us-east-1.amazonaws.com/enrichment \
 -H "Content-Type: application/json" \
 -d '{
--- bin/getEnrichment.sh
+++ bin/getEnrichment.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl -X GET https://9i82rka097.execute-api.us-east-1.amazonaws.com/enrichment/$1 | jq
